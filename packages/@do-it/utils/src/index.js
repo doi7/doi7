3470484e1a347ed3fe157b489ed28cd89bd98961
Antério Vieira <anteriovieira@gmail.com>
2020-04-20 10:36:05 -0300

    fix: adds loadEnv from index file

--- packages/@do-it/utils/src/index.js
+++ packages/@do-it/utils/src/index.js
@@ -3,6 +3,7 @@ const isFunction = val => typeof val === 'function'
 module.exports = {
   isFunction,
   chalk: require('chalk'),
+  loadEnv: require('./lib/load-env'),
   sequence: require('./lib/sequence'),
   logger: require('./lib/logger'),
   notify: require('./lib/notify'),
