914efa53fbef762ea7031d7706070d41f90e0b48
Herberts Cruz <herbertscruz@gmail.com>
2020-04-20 17:18:06 -0300

    Bug fixes

--- packages/@do-it/utils/src/lib/load-env.js
+++ packages/@do-it/utils/src/lib/load-env.js
@@ -1,15 +1,17 @@
+const fs = require('fs');
 const path = require('path')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 
-module.exports = (mode, context = '.') => {
-  const basePath = path.resolve(context, `.env${mode ? `.${mode}` : ``}`)
-  const localPath = `${basePath}.local`
-
+module.exports = ({mode = null, context = '.', local = true}) => {
+  const basePath = path.resolve(context, '.env');
+  const localPath = `${basePath}.local`;
+  const modePath = `${basePath}.${mode}`;
+  
   const load = envPath => {
     try {
       const env = dotenv.config({
-        path: envPath
+        path: envPath, debug: !!process.env.DEBUG ? true : null
       })
 
       dotenvExpand(env)
@@ -21,6 +23,7 @@ module.exports = (mode, context = '.') => {
     }
   }
 
-  load(localPath)
-  load(basePath)
+  if (fs.existsSync(modePath)) load(modePath)
+  if (local && fs.existsSync(localPath)) load(localPath)
+  if (fs.existsSync(basePath)) load(basePath)
 }
---
+++ packages/test/load-env.js
@@ -0,0 +1,7 @@
+require('../@do-it/utils/src/index').loadEnv({
+    mode: 'production',
+    context: __dirname,
+    local: false
+});
+console.log('TEST_A=', process.env.TEST_A);
+console.log('TEST_B=', process.env.TEST_B);
