21569c7a30527b8c7225a33bb1296448ef85ea19
Antério Vieira <anteriovieira@gmail.com>
2020-05-20 12:05:13 -0300

    chore: improvemsnts

--- packages/@do-it/cli/src/commands/task.js
+++ packages/@do-it/cli/src/commands/task.js
@@ -30,6 +30,7 @@ module.exports = async (params, config, env) => {
       task = tasks.find(task => task.key === params.command)
     }
 
+    log('\n', chalk.cyanBright(`🚀 ${task.title} 🚀`), '\n')
     await sequence((task && task.commands) || [], async command => {
       const result = isFunction(command) 
         ? await command({execa, inquirer, task, params, config, env, output })
