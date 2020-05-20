1ce383176d372dd70e96f6284ce10019f961703a
Antério Vieira <anteriovieira@gmail.com>
2020-05-20 11:21:01 -0300

    chore: improvements

--- packages/test/doitfile.js
+++ packages/test/doitfile.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const bash = (cli, context = []) => new Proxy({}, {
+  get: (_, command) => (...args) => [cli, [...context, command, ...args]]
+})
+
+const git = bash('git')
+
+module.exports = {
+  tasks: [
+    {
+      title: 'Show git status',
+      key: 'my-task',
+      commands: [
+        () => {
+          return git.status()
+        }
+      ]
+    }
+  ]
+}
